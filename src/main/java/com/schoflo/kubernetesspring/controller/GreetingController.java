e4ce4c17773ccc391966b3f0263eb4fe5ebed96e
schoflo <flo.scholz@gmx.net>
2023-10-22 19:57:02 +0200

    keycloak anbindung #2

--- src/main/java/com/schoflo/kubernetesspring/controller/GreetingController.java
+++ src/main/java/com/schoflo/kubernetesspring/controller/GreetingController.java
@@ -12,4 +12,9 @@ public class GreetingController {
         return "Hello " + name;
     }
 
+    @GetMapping("/greeting-admin")
+    public String greetingAdmin(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
+        return "Hello " + name;
+    }
+
 }
