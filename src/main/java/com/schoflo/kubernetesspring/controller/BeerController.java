ce5c3b40ef177cab4cc3d009fb7c68eedb49ed28
schoflo <flo.scholz@gmx.net>
2023-10-23 21:28:42 +0200

    database config und beispiel entity

--- src/main/java/com/schoflo/kubernetesspring/controller/BeerController.java
+++ src/main/java/com/schoflo/kubernetesspring/controller/BeerController.java
@@ -1,11 +1,16 @@
 package com.schoflo.kubernetesspring.controller;
 
+import com.schoflo.kubernetesspring.entity.Beer;
+import com.schoflo.kubernetesspring.repository.BeerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class BeerController {
+
+    BeerRepository beerRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
@@ -15,6 +20,11 @@ public class GreetingController {
     @GetMapping("/greeting-admin")
     public String greetingAdmin(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
         return "Hello " + name;
+    }
+
+    @PostMapping("/createBeer")
+    public Beer createBeer(@RequestParam(name = "name", required = false, defaultValue = "Siegel") String name) {
+        return beerRepo.save(new Beer(name, "Union", 4.9));
     }
 
 }
