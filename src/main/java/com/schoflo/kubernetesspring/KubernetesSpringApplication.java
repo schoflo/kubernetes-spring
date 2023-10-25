9da4b47993155bb497ebac9ca033ecc4cd515979
schoflo <flo.scholz@gmx.net>
2023-10-25 19:18:38 +0200

    added mysqldriver. excluded DataSourceAutoConfig

--- src/main/java/com/schoflo/kubernetesspring/KubernetesSpringApplication.java
+++ src/main/java/com/schoflo/kubernetesspring/KubernetesSpringApplication.java
@@ -2,8 +2,9 @@ package com.schoflo.kubernetesspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class KubernetesSpringApplication {
 
     public static void main(String[] args) {
