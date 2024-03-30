17e42aad4cbb8bade6f93a644f2b3590aa9abb2a
schoflo <flo.scholz@gmx.net>
2024-03-30 12:07:04 +0100

    Exceptionhandling vereinfacht

--- business-service/src/main/java/com/schoflo/kubernetesspring/exception/ExceptionHandlingController.java
+++ business-service/src/main/java/com/schoflo/kubernetesspring/exception/ExceptionHandlingController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -12,16 +13,15 @@ public class ExceptionHandlingController {
 
     @ExceptionHandler({BoardgameNotFoundException.class, RowingSessionNotFoundException.class,
             RowingIntervalNotFoundException.class})
-    public ResponseEntity<NotFoundException> notFoundException(NotFoundException ex) {
-        NotFoundException message = new NotFoundException(ex.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> notFoundException(NotFoundException ex) {
         log.debug(ex.getMessage(), ex);
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(BoardgameImageCreationException.class)
-    public ResponseEntity<BoardgameImageCreationException> boardgameImageCreationException(BoardgameImageCreationException ex) {
-        BoardgameImageCreationException message = new BoardgameImageCreationException(ex.getMessage());
+    public ResponseEntity<Object> boardgameImageCreationException(BoardgameImageCreationException ex) {
         log.debug(ex.getMessage(), ex);
-        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 }
