5afd1fc9137d66888fe15a7658530870932102f2
schoflo <flo.scholz@gmx.net>
2025-01-04 13:25:30 +0100

    SecurityConfig angepasst

--- business-service/src/main/java/com/schoflo/kubernetesspring/config/security/SecurityConfig.java
+++ business-service/src/main/java/com/schoflo/kubernetesspring/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -15,19 +16,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-//Als Alternative zu @PreAuthorize kann man auch hier die Endpunkte sichern
-//                        .requestMatchers("rowing/getRowingSessions")
-//                        .hasRole("user")
                         .requestMatchers("/openapi/**").permitAll()
-                        .anyRequest().permitAll());
-//                        .anyRequest().authenticated());
-        http
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/oauth2/authorization/keycloak"))
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/oauth2/authorization/keycloak")));
+
+        http.oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
         return http.build();
     }
-
 }
